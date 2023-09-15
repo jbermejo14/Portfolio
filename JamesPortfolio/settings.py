056dc5da21568fc3d1a1729ccb0877af30884864
jbermejo14 <jpbermejobramley@gmail.com>
2023-09-15 10:05:52 +0200

    Added IP in Settings

--- JamesPortfolio/settings.py
+++ JamesPortfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ik2phm4pn_ogf-^!8fv)h0%-rlttokzm&+7t2epsw*#6pj#l+2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '18.209.150.156']
 
 
 # Application definition
